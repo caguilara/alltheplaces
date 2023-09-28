119dbe985c48bfab98112b844af9f27ced4e59b6
caguilara <caguilara>
2023-09-28 12:46:00 -0300

    Add category for alh_hotels_au spider

--- locations/spiders/alh_hotels_au.py
+++ locations/spiders/alh_hotels_au.py
@@ -1,7 +1,12 @@
 from locations.storefinders.storelocatorwidgets import StoreLocatorWidgetsSpider
+from locations.categories import Categories, apply_category
 
 
 class ALHHotelsAUAUSpider(StoreLocatorWidgetsSpider):
     name = "alh_hotels_au"
     item_attributes = {"brand": "ALH Hotels", "brand_wikidata": "Q119159708"}
     key = "41fdc8f98fc2738172250baa676b369d"
+
+    def parse_item(self, item, location):
+        apply_category(Categories.HOTEL, item)
+        yield item
