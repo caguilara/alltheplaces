ee546c1c4872cc225ef3ea014fc9582601ac4305
Caro Aguilar <48287964+caguilara@users.noreply.github.com>
2023-10-03 15:37:39 -0300

    add category for hairhouse_au

--- locations/spiders/hairhouse_au.py
+++ locations/spiders/hairhouse_au.py
@@ -1,16 +1,25 @@
-from scrapy import Spider
+import scrapy
 
 from locations.dict_parser import DictParser
-
-from locations.hours import DAYS_FULL, OpeningHours
-from scrapy.spiders import SitemapSpider
 from locations.hours import OpeningHours
 
-
-class HairhouseAUSpider(SitemapSpider):
+class HairhouseAUSpider(scrapy.Spider):
     name = "hairhouse_au"
     item_attributes = {"brand": "Hairhouse", "brand_wikidata": "Q118383855"}
     allowed_domains = ["www.hairhouse.com.au"]
     sitemap_urls = ["https://www.hairhouse.com.au/store/sitemap.xml"]
-    sitemap_rules = [("/store/", "parse")]
+    start_urls = [
+        "https://www.hairhouse.com.au/api/storeList?latitude=-37&longitude=144&distance=4000&top=120&_data=routes/($lang)/api/storeList"
+    ]
     custom_settings = {"ROBOTSTXT_OBEY": False}
+
+    def parse(self, response):
+        for location in response.json():
+            item = DictParser.parse(location)
+            item["website"] = "https://www.hairhouse.com.au/stores/" + location["url_component"]
+            hours_string = " ".join(
+                [day_hours["day"] + ": " + day_hours["hours"] for day_hours in location["operatingHours"]]
+            )
+            item["opening_hours"] = OpeningHours()
+            item["opening_hours"].add_ranges_from_string(hours_string)
+            yield item
